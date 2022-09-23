11ddf0589498aef2b12555019b00b6ba78931168
Przemyslaw Mejna <pmejna@gmail.com>
2022-09-23 22:47:50 +0200

    build(layout-view): create home view and main layout (#1)

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import HomeView from '../views/HomeView/HomeView'
+
+const Home: NextPage = () => {
+  return (
+    <HomeView />
+  )
+}
+
+export default Home
