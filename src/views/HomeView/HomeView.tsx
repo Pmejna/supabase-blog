69ed1288f77c1b08c146f175efdd771756c1b6d5
Przemyslaw Mejna <pmejna@gmail.com>
2022-09-24 23:59:21 +0200

    feat(home-view): add type to the index page

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -2,11 +2,11 @@ import type { FC } from "react";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
 import type { HomeProps } from "../../pages";
  
-const HomeView: FC<HomeProps> = ({pageData}) => {
+const HomeView: FC<HomeProps> = ({postsData}) => {
     return ( 
         <MainLayout>
             <h1>HomeView</h1>
-            <pre>{JSON.stringify(pageData, null, 2)}</pre>
+            <pre>{JSON.stringify(postsData, null, 2)}</pre>
         </MainLayout>
      );
 }
